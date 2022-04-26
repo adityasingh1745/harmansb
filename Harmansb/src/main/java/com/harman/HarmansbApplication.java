1312c9d94ea16bbcf4d50d551dca1ffb3a2df8d6
ASingh1 <ASingh1@INBAWN166769.ad.harman.com>
2022-04-26 15:27:28 +0530

    initial commit

---
+++ Harmansb/src/main/java/com/harman/HarmansbApplication.java
@@ -0,0 +1,14 @@
+package com.harman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmansbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmansbApplication.class, args);
+		System.out.println("My First Spring Boot Application");
+	}
+
+}
